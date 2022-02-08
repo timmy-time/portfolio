4dd2748eef577cf832fd5f24a0fb72674207f5e1
TimmyTime <tgree268@eq.edu.au>
2022-02-08 21:40:00 +1000

    Initial Portfolio

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const projects = [
+  {
+    title: "Momento",
+    subtitle: "Advanced Discord Bot",
+    description:
+      "Momento is an open source, fully functional Discord bot that includes moderation, reaction roles, music features, and so much more.",
+    image: "./Momento.png",
+    link: "https://github.com/momentodevs/momento",
+  }
+];
+
+export const testimonials = [
+  {
+    quote:
+      "We're in the business of supporting people all over the world who want to build digital socially relevant products. We are totally open, we don't need anything from anyone.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Aleksei Dolgikh",
+    company: "UnicornWitnesses.com",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "Discord.js Bots",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL", 
+  "Material UI",
+];

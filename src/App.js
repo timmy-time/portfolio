3c7dcf459a9058b286a450ae3877d57d8410786d
TimmyTime <tgree268@eq.edu.au>
2022-02-11 18:51:47 +1000

    Added Paypal Donation Button

--- src/App.js
+++ src/App.js
@@ -16,6 +16,21 @@ export default function App() {
       <Skills />
       {/* <Testimonials /> */}
       <Contact />
+      <div id="donate-button-container">
+      <div id="donate-button"></div>
+      <script src="https://www.paypalobjects.com/donate/sdk/donate-sdk.js" charset="UTF-8"></script>
+      <script>
+      PayPal.Donation.Button({
+      env:'production',
+      hosted_button_id:'KBH82NQY4WS5A',
+      image: {
+      src:'https://www.paypalobjects.com/en_AU/i/btn/btn_donate_LG.gif',
+      alt:'Donate with PayPal button',
+      title:'PayPal - The safer, easier way to pay online!',
+      }
+      }).render('#donate-button');
+      </script>
+      </div>
     </main>
   );
 }

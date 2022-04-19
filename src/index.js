2d09cb3cb4cf0d461ff9ffee46743baca540bc0d
TimmyTime <tgree268@eq.edu.au>
2022-04-19 19:40:27 +1000

    attempt 2

--- src/index.js
+++ src/index.js
@@ -7,14 +7,12 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-import Donate from "./components/Donate";
 
 
 ReactDOM.render(
   <BrowserRouter basename="/">
     <Routes>
       <Route exact path="/" element={<App />}/>
-      <Route path="/donate" element={<Donate />}/>
     </Routes>
   </BrowserRouter>,
   document.getElementById('root')
